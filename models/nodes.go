b255db67208e478700287ac1f385e3d6d6f6f903
sleepinggenius2 <sleepinggenius2@users.noreply.github.com>
2017-09-08 02:25:42 -0400

    Adds initial version of models for generated code

---
+++ models/nodes.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"github.com/sleepinggenius2/gosmi"
+	"github.com/sleepinggenius2/gosmi/types"
+)
+
+type BaseNode struct {
+	Name   string
+	Oid    []uint
+	OidLen uint
+}
+
+type ScalarNode struct {
+	BaseNode
+	Type gosmi.Type
+}
+
+type TableNode struct {
+	BaseNode
+	Row RowNode
+}
+
+func (t TableNode) Columns() []ColumnNode {
+	return t.Row.Columns
+}
+
+func (t TableNode) Index() []ScalarNode {
+	return t.Row.Index
+}
+
+type RowNode struct {
+	BaseNode
+	Columns []ColumnNode
+	Index   []ScalarNode
+}
+
+type ColumnNode struct {
+	ScalarNode
+}
+
+type NotificationNode struct {
+	BaseNode
+}
+
+type NodeType struct {
+	BaseType  types.BaseType
+	Enum      *gosmi.Enum
+	Format    string
+	Formatter gosmi.ValueFormatter
+	Name      string
+	Ranges    []gosmi.Range
+}
